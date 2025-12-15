40004ea831f8552e7e85a4aa3008b3321e69435a
dhanushkumar0080 <dhanushkumar0080@gmail.com>
2025-12-15 15:00:24 +0530

    Update Q6.cpp

--- Q6.cpp
+++ Q6.cpp
@@ -4,13 +4,7 @@
 using namespace std;
 int main()
 {
-    int num;
-    float fact=1;
-    cout<<"Enter a number : ";
-    cin>>num;
-    for(int i=1;i<=num;i++){
-        fact*=i;
-    }
-    cout<<"Factorial : "<<fixed<<setprecision(2)<<fact;
-    return 0;
+  double num;
+  cin>>num;
+  cout<<fixed<<setprecision(3)<<num;
 }
