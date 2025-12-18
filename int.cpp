443bb5ce4250ff0bb1c35577010f3aea11a7a06d
dhanushkumar0080 <dhanushkumar0080@gmail.com>
2025-12-18 14:38:48 +0530

    Update int.cpp

--- int.cpp
+++ int.cpp
@@ -6,7 +6,13 @@ int main(){
     cin>>size;             
         int arr[size];
         for(int i=0;i<size;i++){
-            cin>>arr[i]%==0;
+            cin>>arr[i];
         }
-
+        for(int i=size-1;i>=0;i--){
+            if(i==0){
+			cout<<arr[i];
+			break;
+        }
+        cout<<arr[i]<<",";
+}
     }
